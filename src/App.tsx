8a6bebb3a1d558b2889a3d1a238308546426f1f0
Wellington Silva <Wellingtontp0@gmail.com>
2022-09-16 17:48:57 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+export function App () {
+  return (
+    <body>
+      <div className="">
+    <h1 className="text-3xl font-bold underline text-red-600">lmao</h1>
+  </div>
+
+
+</body>
+  )
+}
+export default App
