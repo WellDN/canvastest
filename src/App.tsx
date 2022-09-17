6522f3b7ca5a1853b40d083a1d377e8922d16bc4
Wellington Silva <Wellingtontp0@gmail.com>
2022-09-17 19:31:45 -0300

    :octocat:

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,69 @@
+import { useEffect, useRef, useState } from "react";
 
-
-export function App () {
-  return (
-    <body>
-      <div className="">
-    <h1 className="text-3xl font-bold underline text-red-600">lmao</h1>
-  </div>
-
-
-</body>
-  )
+class canvasRenderer {  
+    private canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D;
+    private paint: boolean;
+    
+    private clickX: number[] = [];
+    private clickY: number[] = [];
+    private clickDrag: boolean[] = [];
+    constructor() {
+        
+    }
+    
 }
+
+export function App() {
+
+   const canvasRef = useRef(null); //reference object: it will hold reference to the canvas element
+    const contextRef = useRef(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+
+   useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.width = window.innerWidth * 2;
+    canvas.style.height = `${window.innerWidth}px`;     //pixel density
+    canvas.style.height = `${window.innerHeight}px`;    //the canvas width make it supports high density widths ex: Macbooks etc
+
+    const context = canvas.getContext("2d");
+    context.scale(2,2)
+    context.lineCap = "round"
+    context.strokeStyle = "black"
+    context.lineWidth = 5
+    contextRef.current = context;
+   }, []) //[] make it trigger only once
+   
+   const startDrawing = ({nativeEvent}) => {
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true)
+    }
+
+    const finishDrawing = () => {
+        contextRef.current.closePath()
+        setIsDrawing(false)
+    }
+
+    const draw = ({nativeEvent}) => {
+        if(!isDrawing) {
+            return 
+        }
+        const {offsetX, offsetY} = nativeEvent;
+        contextRef.current.lineTo(offsetX, offsetY);
+        contextRef.current.stroke()
+    }
+
+
+   return(
+    <canvas 
+    onMouseDown={startDrawing}
+    onMouseUp={finishDrawing}
+    onMouseMove={draw}
+    ref={canvasRef}/>
+   )
+}
+
 export default App
