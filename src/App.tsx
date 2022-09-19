58a53e98cec61e0d8e41ac9a996602d2a2d8a9d5
Wellington Silva <Wellingtontp0@gmail.com>
2022-09-18 21:02:30 -0300

    types

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,70 @@
 import { useEffect, useRef, useState } from "react";
 
-class canvasRenderer {  
-    private canvas: HTMLCanvasElement;
-    private context: CanvasRenderingContext2D;
-    private paint: boolean;
-    
-    private clickX: number[] = [];
-    private clickY: number[] = [];
-    private clickDrag: boolean[] = [];
-    constructor() {
-        
+class CanvasRenderer {
+
+constructor(
+    private canvas: HTMLCanvasElement,
+    private context: CanvasRenderingContext2D,
+    ) {
+
     }
     
 }
 
 export function App() {
-
-   const canvasRef = useRef(null); //reference object: it will hold reference to the canvas element
+    
+    const canvasRef = useRef<HTMLCanvasElement>(null); //reference object: it will hold reference to the canvas element
     const contextRef = useRef(null);
     const [isDrawing, setIsDrawing] = useState(false);
-
-   useEffect(() => {
-    const canvas = canvasRef.current;
+    
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (canvas == null) throw new Error("could not get canvas") //
+    canvas.width = window.innerWidth * 2;
     canvas.width = window.innerWidth * 2;
     canvas.width = window.innerWidth * 2;
     canvas.style.height = `${window.innerWidth}px`;     //pixel density
     canvas.style.height = `${window.innerHeight}px`;    //the canvas width make it supports high density widths ex: Macbooks etc
-
+            
     const context = canvas.getContext("2d");
+    if (context == null) throw new Error("Could not get context");
     context.scale(2,2)
     context.lineCap = "round"
     context.strokeStyle = "black"
     context.lineWidth = 5
     contextRef.current = context;
-   }, []) //[] make it trigger only once
-   
-   const startDrawing = ({nativeEvent}) => {
+    
+    }, []) //[] make it trigger only once
+
+const startDrawing = ({nativeEvent}) => {
     const {offsetX, offsetY} = nativeEvent;
     contextRef.current.beginPath()
     contextRef.current.moveTo(offsetX, offsetY);
     setIsDrawing(true)
-    }
+}
 
-    const finishDrawing = () => {
-        contextRef.current.closePath()
-        setIsDrawing(false)
-    }
+const finishDrawing = () => {
+    contextRef.current.closePath()
+    setIsDrawing(false)
+}
 
-    const draw = ({nativeEvent}) => {
-        if(!isDrawing) {
-            return 
-        }
-        const {offsetX, offsetY} = nativeEvent;
-        contextRef.current.lineTo(offsetX, offsetY);
-        contextRef.current.stroke()
+const draw = ({nativeEvent}) => {
+    if(!isDrawing) {
+        return 
+    }
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke()
     }
 
 
    return(
-    <canvas 
-    onMouseDown={startDrawing}
-    onMouseUp={finishDrawing}
-    onMouseMove={draw}
-    ref={canvasRef}/>
-   )
-}
-
-export default App
+       <canvas 
+       onMouseDown={startDrawing}
+       onMouseUp={finishDrawing}
+       onMouseMove={draw}
+       ref={canvasRef}/>
+       )
+    }
+    
+    export default App
